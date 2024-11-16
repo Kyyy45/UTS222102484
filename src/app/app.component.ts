76fb89a33a3e802d3241415630ce559d13c56dd7
Keyyy <156584233+Kyyy45@users.noreply.github.com>
2024-11-16 09:14:48 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidebarComponent, HeaderComponent, EcommerceComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce',
+  standalone: true,
+  imports: [],
+  templateUrl: './ecommerce.component.html',
+  styleUrl: './ecommerce.component.css'
+})
+export class EcommerceComponent {
+}
